893a19e2e494790ae3f5c2d4606276db545d41a5
Ihor <39652204+IhorBalan@users.noreply.github.com>
2020-07-24 16:02:28 +0300

    change UserList to functional component

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -3,21 +3,19 @@ import PropTypes from "prop-types";
 //styles
 import "./UserList.scss";
 
-class UserList extends React.Component {
-  render() {
-    return (
-      <div className="user-list__list">
-        {this.props.users.map((user) => {
-          return (
-            <div className="user-list__item" key={user.id}>
-              {user.firstName} {user.lastName}
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
+const UserList = ({ users }) => {
+  return (
+    <div className="user-list__list">
+      {users.map((user) => {
+        return (
+          <div className="user-list__item" key={user.id}>
+            {user.firstName} {user.lastName}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 UserList.propTypes = {
   users: PropTypes.array,
